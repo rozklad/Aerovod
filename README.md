3c60541f3139e85e1b39f9a12b22bbd5263b71d9
rozklad <jan.rozklad@gmail.com>
2015-01-26 21:14:58 +0100

    gulp

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Aerovod
+
+## Dev dependencies
+
+	npm install gulp
+	npm install gulp-sass
+
+## Gulp tasks
+
+- **sass** - produce css from scss sources
+- **watch** - run ``sass`` task on change
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// npm install gulp
+// npm install gulp-sass
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./assets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/scss/*.scss', ['sass']);
+});
